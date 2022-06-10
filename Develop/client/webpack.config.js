900117c40c5fb80d7860a9bda46ead94fb94c5fe
Zephyr <phillip.t.mock@gmail.com>
2022-06-09 17:44:23 -0700

    client side js and webpack config

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -18,12 +18,55 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+// the following webpack plugin will inject the needed bundles and generate the html file.
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'J.A.T.E.'
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'J.A.T.E.',
+        short_name: 'Jate',
+        description: 'text editor skelly',
+        background_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+
     ],
 
     module: {
       rules: [
-        
+       {
+         test: /\.css$/i,
+         use: ['style-loader', 'css-loader'],
+       },
+       {
+         test: /\.m?js$/,
+         exclude: /node_modules/,
+         use: {
+           loader: 'babel-loader',
+           options: {
+             presets: ['@babel/preset-env'],
+             plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+           },
+         },
+       },
+       
       ],
     },
   };
